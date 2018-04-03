de54e6a2ab6e5c011da1925c8b1cca15e1691045
Dylan Richards <drichards@TD1001562.local>
2018-04-03 14:41:07 -0400

    Implement base_price validation

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,17 @@
 class Book < ApplicationRecord
-  validates_presence_of :title
+  validates_presence_of :title, :release_date
+
+  before_save :check_release_date
 
   has_one :format
+
+  private
+
+  def check_release_date
+    if  (release_date < Date.today + 2.months) && base_price.blank?
+      errors.add(:base_price, "base_price can't be blank when the release date is less than 2 months away.")
+      throw(:abort)
+    end
+  end
 
 end
--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -2,4 +2,32 @@ require 'rails_helper'
 
 describe Book  do
   it { should validate_presence_of(:title)  }
+  it { should validate_presence_of(:release_date)  }
+
+  it 'will not save without a base price if the release date is within 2 months' do
+    book = Book.create(title: "Book", release_date: 1.months.from_now)
+
+    expect {
+      book
+    }.to change(Book, :count).by 0
+
+    expect(book.errors[:base_price]).to include "base_price can't be blank when the release date is less than 2 months away."
+  end
+
+
+  it 'saves without a base price if the release date is greater than 2 months out' do
+    expect {
+      Book.create(title: "Book", release_date: 3.months.from_now)
+    }.to change(Book, :count).by 1
+  end
+
+  it 'saves with a base price' do
+    expect {
+      Book.create(title: "Book", release_date: 1.months.from_now, base_price: 1.00)
+    }.to change(Book, :count).by 1
+
+    expect {
+      Book.create(title: "Book", release_date: 3.months.from_now, base_price: 1.00)
+    }.to change(Book, :count).by 1
+  end
 end
